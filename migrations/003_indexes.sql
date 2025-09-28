ec430f534df53a58c30da44ce29e78255507bcf5
Your Name <you@example.com>
2025-09-28 15:04:05 +0900

    chore: stop tracking local DB backups & data; ignore them

--- migrations/003_indexes.sql
+++
@@ -1,3 +0,0 @@
-﻿-- 재실행 안전 예시
-ALTER TABLE users ADD COLUMN IF NOT EXISTS last_login_at DATETIME NULL, ALGORITHM=INPLACE, LOCK=NONE;
-CREATE INDEX IF NOT EXISTS idx_users_last_login ON users(last_login_at);
